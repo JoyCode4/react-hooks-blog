cb8f6d29e3d5067426c0939f180c10de101d275e
JoyCode4 <joywadhonkar4@gmail.com>
2023-05-12 12:13:40 +0530

    create the routes by using react-router-dom library of npm and also render the form data using useState hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
+import { Route, Routes} from "react-router-dom";
+import {Navbar,Home,CreatePost, PostDetail} from "./index";
+
 function App() {
   return (
-    <div className="App">
-      <h1>React application</h1>
+    <div className="App container">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/post/:postId" element={<PostDetail/>}/>
+        <Route path="/create-post" element={<CreatePost/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar";
+import PostDetail from "./PostDetail";
+import Home from "./Home";
+import CreatePost from "./CreatePost";
+
+export {
+    Navbar,
+    Home,
+    CreatePost,
+    PostDetail
+}
