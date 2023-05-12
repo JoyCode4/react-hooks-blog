9a15175174e46a291ad020f5ac2b6054455808ca
JoyCode4 <joywadhonkar4@gmail.com>
2023-05-12 13:01:59 +0530

    get the post using params or link of the route and show details

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,7 +1,20 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { db } from "../firebase";
+
 function PostDetail(){
+    const [post,setPost] = useState({});
+    const postId= useParams().postId;
+    useEffect(()=>{
+        db.collection("posts").doc(postId).get().then((snapshot)=>{
+            // console.log("snapshot",snapshot.data());
+            setPost(snapshot.data());
+        })
+    },[])
     return (
-        <div className="">
-            PostDetail
+        <div className="post-detail">
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
         </div>
     )
 }
